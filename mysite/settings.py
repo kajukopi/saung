98013deeb90cfb83d08fa9c90155cf09ef4ccb24
kajukopi <kajukopi@gmail.com>
2021-07-14 13:44:25 +0700

    Repair static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,14 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_URL = '/static/'
-    
-if not DEBUG:
-    STATIC_ROOT = ''
-    
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/") # correct
 
 
 # Default primary key field type
